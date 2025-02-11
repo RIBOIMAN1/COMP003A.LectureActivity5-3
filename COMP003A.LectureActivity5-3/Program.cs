7acb22965882e0887059300d7d962788ea57b68d
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-10 22:21:43 -0800

    Added code from step 5 of the 5.8 lecture activity.

--- COMP003A.LectureActivity5-3/Program.cs
+++ COMP003A.LectureActivity5-3/Program.cs
@@ -92,6 +92,79 @@ namespace COMP003A.LectureActivity5_3
 			/// </summary>
 			void Draw();
 		}
+		/// <summary>
+		/// Represents a circle derived from Shape and implements IDrawable
+		/// </summary>
+		class Circle : Shape, IDrawable
+		{
+			// Auto-implemented property
+			public double Radius { get; set; }
+
+			/// <summary>
+			/// Constructor for Circle.
+			/// </summary>
+			/// <param name="radius"></param>
+			public Circle(double radius)
+			{
+				Name = "Circle";
+				Radius = radius;
+			}
+
+			/// <summary>
+			/// Calculates the area of the circle.
+			/// </summary>
+			public override double CalculateArea()
+			{
+				// returns the area of the circle
+				return Math.PI * Radius * Radius;
+			}
+
+			/// <summary>
+			/// Implements Draw() method for a circle.
+			/// </summary>
+			public void Draw()
+			{
+				Console.WriteLine("Drawing a circle.");
+			}
+		}
+		/// <summary>
+		/// Represents a rectangle derived from Shape and implements IDrawable.
+		/// </summary>
+		class Rectangle : Shape, IDrawable
+		{
+			// Auto-implemented properties
+			public double Width { get; set; }
+			public double Height { get; set; }
+
+			/// <summary>
+			/// Constructor for Rectangle.
+			/// </summary>
+			/// <param name="width"></param>
+			/// <param name="height"></param>
+			public Rectangle(double width, double height)
+			{
+				Name = "Rectangle";
+				Width = width;
+				Height = height;
+			}
+
+			/// <summary>
+			/// Calculates the area of the rectangle.
+			/// </summary>
+			public override double CalculateArea()
+			{
+				// returns the area of the rectangle
+				return Width * Height;
+			}
+
+			/// <summary>
+			/// Implements Draw() method for a rectangle.
+			/// </summary>
+			public void Draw()
+			{
+				Console.WriteLine("Drawing a rectangle.");
+			}
+		}
 	}
 	internal class Program
     {
