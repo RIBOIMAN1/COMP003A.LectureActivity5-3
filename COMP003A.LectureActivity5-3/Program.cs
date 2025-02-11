0c1f0d42b80d72d62a0b5b88be8bc497117772ff
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-10 22:00:59 -0800

    Added code from step 3 of the 5.8 lecture activity.

--- COMP003A.LectureActivity5-3/Program.cs
+++ COMP003A.LectureActivity5-3/Program.cs
@@ -26,6 +26,63 @@ namespace COMP003A.LectureActivity5_3
 			Console.WriteLine($"Shape: {Name}");
 		}
 	}
+	/// <summary>
+	/// Represents a circle derived from Shape.
+	/// </summary>
+	class Circle : Shape
+	{
+		// Auto-implemented property
+		public double Radius { get; set; }
+
+		/// <summary>
+		/// Constructor for Circle.
+		/// </summary>
+		/// <param name="radius"></param>
+		public Circle(double radius)
+		{
+			Name = "Circle";
+			Radius = radius;
+		}
+
+		/// <summary>
+		/// Calculates the area of the circle.
+		/// </summary>
+		public override double CalculateArea()
+		{
+			// returns the area of the circle
+			return Math.PI * Radius * Radius;
+		}
+	}
+	/// <summary>
+	/// Represents a rectangle derived from Shape.
+	/// </summary>
+	class Rectangle : Shape
+	{
+		// Auto-implemented properties
+		public double Width { get; set; }
+		public double Height { get; set; }
+
+		/// <summary>
+		/// Constructor for Rectangle.
+		/// </summary>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		public Rectangle(double width, double height)
+		{
+			Name = "Rectangle";
+			Width = width;
+			Height = height;
+		}
+
+		/// <summary>
+		/// Calculates the area of the rectangle.
+		/// </summary>
+		public override double CalculateArea()
+		{
+			// returns the area of the rectangle
+			return Width * Height;
+		}
+	}
 	internal class Program
     {
         static void Main(string[] args)
